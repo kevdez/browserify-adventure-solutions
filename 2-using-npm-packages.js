197754b242d2316850a234bf05239bea43574d1e
Kevin Hernandez <khernan3@uci.edu>
2015-07-27 20:44:09 -0700

    2. Using NPM Packages - Complete

---
+++ 2-using-npm-packages.js
@@ -0,0 +1,10 @@
+var uniq = require('uniq');
+
+console.log(uniq(prompt().split(',')));
+
+
+// neater solution: 
+// var uniq = require('uniq');
+// var list = prompt('enter a list').split(',');
+// console.log(uniq(list));
+
